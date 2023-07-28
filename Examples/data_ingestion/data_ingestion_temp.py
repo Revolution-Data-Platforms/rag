2c2fd30d4f743df8d68beeb5c6b6b2c1aa944c1c
Max <22856173+Max-FM@users.noreply.github.com>
2023-07-28 18:02:08 +0000

    Create temporary data ingestion script for testing and development

--- Examples/data_ingestion/data_ingestion_temp.py
+++ Examples/data_ingestion/data_ingestion_temp.py
@@ -1,7 +1,8 @@
 # %%
 # setting path
 import sys
-sys.path.append('../../')
+
+sys.path.append("../../")
 
 from core.DocumentFactory import DocumentFactory
 from langchain.embeddings import HuggingFaceInstructEmbeddings
@@ -9,13 +10,13 @@ from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 import os
 
-load_dotenv('./data_ingestion.env')
+load_dotenv("./data_ingestion.env")
 
 # Gettting environment variables
-drop_zone_location   = os.environ.get('drop_zone_location')
-vectorstore_location = os.environ.get('vectorstore_location')
-vectorstore_name     = os.environ.get('vectorstore_name')
-embedding_model_name = os.environ.get('embedding_model_name')
+drop_zone_location = "/home/mfoxleym/Ciena-Gen-AI/data/uaa-publications-23.04/MyWebHelp"  # os.environ.get('drop_zone_location')
+vectorstore_location = os.environ.get("vectorstore_location")
+vectorstore_name = os.environ.get("vectorstore_name")
+embedding_model_name = os.environ.get("embedding_model_name")
 
 Factory = DocumentFactory()
 
@@ -23,8 +24,9 @@ Factory = DocumentFactory()
 docs = Factory.load_documents_from_directory(drop_zone_location)
 # Process documents
 texts = Factory.process_documents(docs)
-embedding = HuggingFaceInstructEmbeddings(model_name=embedding_model_name, model_kwargs={'device': 'cuda'})
-
+embedding = HuggingFaceInstructEmbeddings(
+    model_name=embedding_model_name, model_kwargs={"device": "cuda"}
+)
 
 
 # %%
@@ -32,4 +34,8 @@ embedding = HuggingFaceInstructEmbeddings(model_name=embedding_model_name, model
 vector_store = FAISS.from_documents(documents=texts, embedding=embedding)
 
 # %%
-vector_store.save_local(f"/home/mfoxleym/Ciena-Gen-AI/vectorstores/test-01")
+vector_store.save_local(
+    f"/home/mfoxleym/Ciena-Gen-AI/data/vectorstores/html-docs"
+)
+
+# %%
