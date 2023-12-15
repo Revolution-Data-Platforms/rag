a4bbeb0a8c47b14cfc870a26a5c993ecf969b3c9
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2023-12-15 04:09:23 +0000

    finally working :)

--- frontend/gui_gradio.py
+++ frontend/gui_gradio.py
@@ -1,8 +1,88 @@
 import gradio as gr
 import os
 import time
-
+from backend.llm.baseLLM import Remote_LLM 
 # Chatbot demo with multimodal input (text, markdown, LaTeX, code blocks, image, audio, & video). Plus shows support for streaming text.
+
+
+from backend.retrieval.ciena_retreival import CienaRetrieval
+from backend.embedder.baseEmbedder import baseEmbedder
+from backend.retrieval.utils import *
+from backend.retrieval.rereanker import Reranker
+from langchain.document_loaders import JSONLoader
+
+embedding_function = baseEmbedder().embedding_function
+retriael_kwargs = {
+    "threshold": "0.8",
+    "k": 20,
+    "embedder": embedding_function,
+    "hybrid": True
+}
+ciena_retreival = CienaRetrieval(**retriael_kwargs)
+reranker = Reranker()
+
+
+def load_db():
+    """Load Ciena database."""
+    
+    dir = './output/'
+    loaded_data = []
+    for r, d, f in os.walk(dir):
+        
+        for file in f:
+            if "10Aug-BP_Engineering_guide" in file or "Blue_Planet_MLA_Cloud_Deployment_Guide" in file:
+                if '.json' in file and file != 'structuredData.json':
+                    dir_ = file.split('.')[0] + '.pdf'
+                    file_name = os.path.join(dir, dir_, file)
+                    print(file_name)
+                    try:
+                        loader = JSONLoader(
+                            file_path=file_name,
+                            jq_schema='.[].content[]',
+                            content_key="text", 
+                            metadata_func=metadata_func)
+
+                        loaded_data.extend(loader.load())
+                        print(f"Successfully loaded file {file_name}")
+                    except:
+                        print(f"error in loading  file {file_name}")
+                        pass
+
+    return loaded_data
+
+
+def clean(docs):
+    loaded_data = filter_empty(docs)
+    loaded_data = filter_redundant(loaded_data)
+    loaded_data = exclude_toc(loaded_data)
+    return loaded_data
+
+
+def get_relevant_docs(query, docs):
+    """Get relevant documents from Ciena database."""
+    ciena_retrieval = CienaRetrieval(**retriael_kwargs)
+    relevant_docs = ciena_retrieval.get_res(query, docs)
+    reranked_res = reranker.rerank(query, relevant_docs)
+    return reranked_res
+
+def get_context(docs, headers):
+    context, sources = ciena_retreival.get_context(docs, headers)
+    return context, sources
+
+
+if __name__ == "__main__":
+    loaded_db = load_db()
+    cleaned_db = clean(loaded_db)
+    query = "Table BPO Runtime License"
+    relevant_docs = get_relevant_docs(query, loaded_db)
+    rel_headers = relevant_headers(relevant_docs)
+    import pdb; pdb.set_trace()
+    context, sources = get_context(loaded_db, rel_headers)
+    print(context)
+
+
+
+
 
 def add_text(history, text):
     history = history + [(text, None)]
---
+++ gui_gradio.py
@@ -0,0 +1,126 @@
+import gradio as gr
+import os
+from asgiref.sync import sync_to_async
+from backend.llm.baseLLM import Remote_LLM 
+# Chatbot demo with multimodal input (text, markdown, LaTeX, code blocks, image, audio, & video). Plus shows support for streaming text.
+from backend.retrieval.ciena_retreival import CienaRetrieval
+from backend.embedder.baseEmbedder import baseEmbedder
+from backend.retrieval.utils import *
+from backend.retrieval.rereanker import Reranker
+from langchain.document_loaders import JSONLoader
+
+import asyncio
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+_executor = ThreadPoolExecutor(1)
+
+
+
+embedding_function = baseEmbedder().embedding_function
+retriael_kwargs = {
+    "threshold": "0.8",
+    "k": 20,
+    "embedder": embedding_function,
+    "hybrid": True
+}
+ciena_retreival = CienaRetrieval(**retriael_kwargs)
+reranker = Reranker()
+
+def load_db():
+    """Load Ciena database."""
+    
+    dir = './output/'
+    loaded_data = []
+    for r, d, f in os.walk(dir):
+        
+        for file in f:
+            # if "10Aug-BP_Engineering_guide" in file or "Blue_Planet_MLA_Cloud_Deployment_Guide" in file:
+            if '.json' in file and file != 'structuredData.json':
+                dir_ = file.split('.')[0] + '.pdf'
+                file_name = os.path.join(dir, dir_, file)
+                print(file_name)
+                try:
+                    loader = JSONLoader(
+                        file_path=file_name,
+                        jq_schema='.[].content[]',
+                        content_key="text", 
+                        metadata_func=metadata_func)
+
+                    loaded_data.extend(loader.load())
+                    print(f"Successfully loaded file {file_name}")
+                except Exception as e:
+                    # import pdb; pdb.set_trace()
+                    print(f"error in loading  file {file_name}")
+                    print(e)
+
+    return loaded_data
+
+def clean(docs):
+    loaded_data = filter_empty(docs)
+    loaded_data = filter_redundant(loaded_data)
+    loaded_data = exclude_toc(loaded_data)
+    return loaded_data
+
+
+def get_relevant_docs(query, docs):
+    """Get relevant documents from Ciena database."""
+    ciena_retrieval = CienaRetrieval(**retriael_kwargs)
+    relevant_docs = ciena_retrieval.get_res(query, docs)
+    reranked_res = reranker.rerank(query, relevant_docs)
+    return reranked_res
+
+def get_context(docs, headers):
+    context, sources = ciena_retreival.get_context(docs, headers)
+    return context, sources
+
+def add_text(history, text):
+    history = history + [(text, None)]
+    return history, gr.Textbox(value="", interactive=False)
+
+def bot(history):
+    response = "**That's cool!**"
+    history[-1][1] = ""
+    for character in response:
+        history[-1][1] += character
+        time.sleep(0.05)
+        yield history
+
+
+loaded_db = load_db()
+cleaned_db = clean(loaded_db)
+
+def main_get_src_ctx(message, seconds):
+    time.sleep(seconds)
+    query = message
+    relevant_docs = get_relevant_docs(query, cleaned_db)
+    rel_headers = relevant_headers(relevant_docs)
+    context, sources = get_context(loaded_db, rel_headers)
+    return context, sources
+
+async def slow_echo(message, history):
+    src, ctx = await sync_to_async(main_get_src_ctx)(message, 1)
+    # src, ctx = await main_get_src_ctx(message)
+    print(src, ctx)
+
+
+def main():
+    gr.ChatInterface(
+            slow_echo,
+            chatbot=gr.Chatbot(height=300),
+            textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
+            title="BP Chatbot",
+            description="Ask Yes Man any question",
+            theme="soft",
+            examples=["Hello", "Am I cool?", "Are tomatoes vegetables?"],
+            cache_examples=True,
+            retry_btn=None,
+            undo_btn="Delete Previous",
+            clear_btn="Clear",
+        ).launch()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
+# if __name__ == "__main__":
+#     main()
